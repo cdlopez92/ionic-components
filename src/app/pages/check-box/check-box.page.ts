863af308b4ff1db3021dc8fa5c79611023b2d55e
cristian.lopez <cristiand.lopezb@hotmail.com>
2021-04-17 09:42:09 -0500

    Agregamos Checkbox

---
+++ src/app/pages/check-box/check-box.page.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-check-box',
+  templateUrl: './check-box.page.html',
+  styleUrls: ['./check-box.page.scss'],
+})
+export class CheckBoxPage implements OnInit {
+
+  data = [
+    {
+      name: 'primary',
+      selected: true
+    },
+    {
+      name: 'secondary',
+      selected: true
+    },
+    {
+      name: 'tertiary',
+      selected: true
+    },
+    {
+      name: 'success',
+      selected: true
+    }
+  
+]
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -33,6 +33,16 @@ export class InicioPage implements OnInit {
       icon: 'radio-button-off-outline',
       name: 'Buttons',
       redirectTo: '/button'
+    },
+    {
+      icon: 'card-outline',
+      name: 'Cards',
+      redirectTo: '/card'
+    },
+    {
+      icon: 'checkmark-circle-outline',
+      name: 'Checks',
+      redirectTo: '/check-box'
     }
   ];
 
